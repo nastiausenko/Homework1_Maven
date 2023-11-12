b52b878df7dc468401b2608e4307b417b45583bd
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-11-12 20:51:54 +0200

    add main functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person("Anastasiia", "Usenko");
 
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
